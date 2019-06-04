6402ae1e8f62031c5cb12191a94d0cb7c7262ce7
Chris Schnaufer <schnaufer@email.arizona.edu>
2019-06-04 15:36:39 -0700

    Handling folder structures

--- test/extract.sh
+++ test/extract.sh
@@ -5,10 +5,24 @@ set -ev
 # Make the folder for the data files
 mkdir data
 
-# Copy and decompress the TAR file
+# Copy the TAR file
 cp "$1" data/
 cd  data
-tar -xv -f "$1"
+
+# Decompress the tar file
+TAR_FILENAME="$1"
+if [[ ! -f "./${TAR_FILENAME}" ]]; then
+    # The copied file doesn't exist - possibly due to folder structure - so we try to find it
+    for fn in *.tar; do
+        [[ -f "${fn}" ]] || break
+        echo "Checking if file ${fn} matches ${TAR_FILENAME}"
+        if [[ "${TAR_FILENAME}" == *"${fn}" ]]; then
+            TAR_FILENAME="${fn}"
+            break
+        fi
+    done
+fi
+tar -xv -f "${TAR_FILENAME}"
 
 # Remove the tar file copy
-rm "$1"
+rm "${TAR_FILENAME}"
--- test/setup_comparables.sh
+++ test/setup_comparables.sh
@@ -8,8 +8,22 @@ mkdir compare
 # Copy and decompress the TAR file
 cp $1 compare/
 cd  compare
-tar -xv -f $1
+
+# Decompress the tar file
+TAR_FILENAME="$1"
+if [[ ! -f "./${TAR_FILENAME}" ]]; then
+    # The copied file doesn't exist - possibly due to folder structure - so we try to find it
+    for fn in *.tar; do
+        [[ -f "${fn}" ]] || break
+        echo "Checking if file ${fn} matches ${TAR_FILENAME}"
+        if [[ "${TAR_FILENAME}" == *"${fn}" ]]; then
+            TAR_FILENAME="${fn}"
+            break
+        fi
+    done
+fi
+tar -xv -f "${TAR_FILENAME}"
 
 # Remove the tar file copy
-rm $1
+rm "${TAR_FILENAME}"
 
